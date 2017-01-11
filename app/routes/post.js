d4370f8dcec66e9cf3fd4d4db534b7af17ab08aa
Erica Nafziger <ericakait@Ericas-iMac.local>
2017-01-11 12:55:49 -0800

    add nav bar and application.hbs file

--- app/routes/post.js
+++ app/routes/post.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(params) {
+  model(params, transition) {
     return this.store.findRecord('posts', params.post_id);
   }
 });
